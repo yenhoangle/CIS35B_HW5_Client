721158c6b6cc7d5159c235170b9bbe7149e01ca9
yenhoangle <yhle@ucsd.edu>
2019-11-25 19:16:11 -0800

    added sample codes into client package + added text files

--- src/client/SelectCarOptions.java
+++ src/client/SelectCarOptions.java
@@ -1,4 +1,37 @@
 package client;
 
+import model.*;
+import java.util.*;
+
 public class SelectCarOptions {
+
+    ////////// PROPERTIES //////////
+    private Scanner in = new Scanner(System.in);
+
+    ////////// CONSTRUCTORS //////////
+
+    public SelectCarOptions() {
+
+    }
+
+    ////////// INSTANCE METHODS //////////
+
+    public void configureAuto(Object obj) {
+
+    }
+
+    public boolean isAutomobile(Object obj) {
+        boolean isAutomobile = false;
+
+        try {
+            Automotive a1 = (Automotive) obj;
+            isAutomobile = true;
+        }
+        catch (ClassCastException e) {
+            isAutomobile = false;
+        }
+
+        return isAutomobile;
+    }
+
 }
