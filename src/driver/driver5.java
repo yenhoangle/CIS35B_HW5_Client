6c1872bcfeff6e987a3302e1be89dae6dfb8a5bb
yenhoangle <yhle@ucsd.edu>
2019-11-29 01:34:58 -0800

    added driver file

--- src/driver/driver5.java
+++ src/driver/driver5.java
@@ -1,9 +1,8 @@
 package driver;
 import client.*;
-import exception.*;
 
 public class driver5 {
-     public static void main(String[] args) throws AutoException {
+     public static void main(String[] args) {
          String localHost = "127.0.0.1";
          int portNum = 4444;
          DefaultSocketClient client = new DefaultSocketClient(localHost, portNum);
